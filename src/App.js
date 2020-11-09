7946c92e1310f21aa2b9d521b766c7f2e7bfb827
Matthew Knop <knopmatthew93@gmail.com>
2020-11-09 00:58:32 -0700

    Added About route path.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 
 /* Import components */
 import Home from './components/Home';
+import About from './components/About';
 import Projects from './components/Projects';
 
 /* Import CSS */
@@ -13,6 +14,7 @@ export default function App() {
         <main>
             <Switch>
                 <Route path="/" component={ Home } exact />
+                <Route path="/about" component= { About } />
                 <Route path="/projects" component={ Projects } />
             </Switch>
         </main>
