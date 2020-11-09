5a9a3b4bb73ca33d4223876fb358a73e518aece1
Matthew Knop <knopmatthew93@gmail.com>
2020-11-09 00:04:04 -0700

    Created route paths.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+/* Import components */
+import Home from './components/Home';
+import Projects from './components/Projects';
+
+/* Import CSS */
 import './App.css';
 
-function App() {
-  return (
-      <div>Hello, World!</div>
-  );
+export default function App() {
+    return (
+        <main>
+            <Switch>
+                <Route path="/" component={ Home } exact />
+                <Route path="/projects" component={ Projects } />
+            </Switch>
+        </main>
+    );
 }
-
-export default App;
