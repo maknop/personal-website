fcef8108ea0ed472082a00bbe3196faab2fc10a1
Matthew Knop <knopmatthew93@gmail.com>
2020-12-13 17:56:31 -0700

    Fixed sh to stop/remove old images.

--- redeploy.sh
+++ redeploy.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-docker build -t personal-website:latest .
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
+
+docker pull maknop/personal-website:latest
 docker run -d -p 127.0.0.1:8000:8000 maknop/personal-website:latest
