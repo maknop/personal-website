4b3a2ac701b1d13d386c2e6800d18ed0ccf21c5a
Matthew Knop <knopmatthew93@gmail.com>
2020-12-13 18:04:54 -0700

    Added login for DockerHub.

--- redeploy.sh
+++ redeploy.sh
@@ -2,5 +2,7 @@
 sudo docker container stop $(docker container ls -aq)
 sudo docker container rm $(docker container ls -aq)
 
+sudo docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_PASSWORD
+
 sudo docker pull maknop/personal-website:latest
 sudo docker run -d -p 127.0.0.1:8000:8000 maknop/personal-website:latest
