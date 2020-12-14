918972bf17136df7ec1e56dcbe6773bbc1ae2a1b
Matthew Knop <knopmatthew93@gmail.com>
2020-12-13 17:58:56 -0700

    Fixed stop/remove commands.

--- redeploy.sh
+++ redeploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
+docker container stop $(docker container ls -aq)
+docker container rm $(docker container ls -aq)
 
 docker pull maknop/personal-website:latest
 docker run -d -p 127.0.0.1:8000:8000 maknop/personal-website:latest
