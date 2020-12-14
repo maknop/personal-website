da41d2af93ad2a894e1882f1f10e2cb510acd36f
Matthew Knop <knopmatthew93@gmail.com>
2020-12-13 17:59:38 -0700

    Added sudo.

--- redeploy.sh
+++ redeploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-docker container stop $(docker container ls -aq)
-docker container rm $(docker container ls -aq)
+sudo docker container stop $(docker container ls -aq)
+sudo docker container rm $(docker container ls -aq)
 
-docker pull maknop/personal-website:latest
-docker run -d -p 127.0.0.1:8000:8000 maknop/personal-website:latest
+sudo docker pull maknop/personal-website:latest
+sudo docker run -d -p 127.0.0.1:8000:8000 maknop/personal-website:latest
